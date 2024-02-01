ce34a1e9480afba1404ed12cc3b93c05ad367b19
keirh80 <khachimarur80@gmail.com>
2024-02-01 16:05:01 +0100

    Improved Graph Layout Algorithm

--- diagram/src/App.vue
+++ diagram/src/App.vue
@@ -19,38 +19,128 @@ export default {
     },
     options: [
       {
-        nodes: ['A', 'B', 'C'],
+        nodes: [{ id: 'A' }, { id: 'B' }, { id: 'C' }],
         edges: [
-          ['A', 'B'],
-          ['A', 'C']
+          ['A' , 'B'],
+          ['A' , 'C']
         ]
       },
       {
-        nodes: ['D', 'E'],
+        nodes: [{ id: 'D' }, { id: 'E' }],
         edges: [
-          ['D', 'E']
+          ['D', 'E' ]
         ]
       },
       {
-        nodes: ['F', 'G', 'H', 'I', 'J'],
+        nodes: [
+          { id: 'A' }, { id: 'B' }, { id: 'C' }, { id: 'D' }, { id: 'E' },
+          { id: 'F' }, { id: 'G' }, { id: 'H' }, { id: 'I' }, { id: 'J' }, { id: 'K' }
+        ],
         edges: [
-          ['F', 'G'],
-          ['F', 'H'],
-          ['H', 'I'],
-          ['H', 'J'],
-          ['J', 'G'],
+          ['A', 'B'], ['A', 'C'], ['B', 'D'], ['C', 'E'],
+          ['D', 'F'], ['D', 'G'], ['E', 'H'], ['F', 'I'],
+          ['G', 'J'], ['H', 'K'], ['I', 'J'], ['J', 'K'],
+          ['F', 'G'], ['G', 'H'], ['H', 'I']
         ]
       },
+      {
+        nodes: [
+          { id: 'A' }, { id: 'B' }, { id: 'C' }, { id: 'D' }, { id: 'E' }, { id: 'F' }, { id: 'G' }, { id: 'H' }, { id: 'I' }, { id: 'J' }, { id: 'K' }
+        ],
+        edges: [
+          ['A', 'B'], ['A', 'C'], ['A', 'D'], ['A', 'E'], ['B', 'F'], ['F', 'G'], ['G', 'H'], ['H', 'I'], ['I', 'J'], ['C', 'K'], 
+        ]
+      },
+      {
+        nodes: [
+          { id: 'X' }, { id: 'Y' }, { id: 'Z' }, { id: 'W' }, { id: 'V' },
+          { id: 'U' }, { id: 'T' }, { id: 'S' }, { id: 'R' }, { id: 'Q' },
+          { id: 'P' }, { id: 'O' }, { id: 'N' }, { id: 'M' }, { id: 'L' },
+          { id: 'K' }, { id: 'J' }, { id: 'I' }, { id: 'H' }, { id: 'G' },
+          { id: 'F' }, { id: 'E' }, { id: 'D' }, { id: 'C' }, { id: 'B' },
+          { id: 'A' }
+        ],
+        edges: [
+          ['A', 'B'], ['A', 'C'], ['B', 'D'], ['C', 'E'],
+          ['D', 'F'], ['D', 'G'], ['E', 'H'], ['F', 'I'],
+          ['G', 'J'], ['H', 'K'], ['I', 'J'], ['J', 'K'],
+          ['K', 'L'], ['L', 'M'], ['M', 'N'], ['N', 'O'],
+          ['O', 'P'], ['P', 'Q'], ['Q', 'R'], ['R', 'S'],
+          ['S', 'T'], ['T', 'U'], ['U', 'V'], ['V', 'W'],
+          ['W', 'X'], ['X', 'Y'], ['Y', 'Z'], ['Z', 'X'],
+          ['Z', 'W'], ['W', 'U'], ['U', 'Q'], ['Q', 'L'],
+          ['L', 'G'], ['G', 'D'], ['D', 'A']
+        ]
+      },
+      {
+        nodes: [
+          { id: 'S' },
+          { id: 'Q' },
+          { id: 'V' },
+          { id: 'C' },
+          { id: 'N' },
+          { id: 'E' }
+        ],
+        edges: [
+          ['S', 'Q'],
+          ['Q', 'V'],
+          ['Q', 'C'],
+          ['C', 'N'],
+          ['N', 'V'],
+          ['C', 'V'],
+          ['V', 'E']
+        ]
+      },
+      {
+        nodes: [
+          { id: 'A' }, { id: 'B' }, { id: 'C' }, { id: 'D' },
+          { id: 'E' }, { id: 'F' }, { id: 'G' }, { id: 'H' }
+        ],
+        edges: [
+          ['A', 'B'], ['A', 'C'], ['B', 'D'], ['C', 'E'],
+          ['D', 'F'], ['E', 'G'], ['F', 'H'], ['G', 'H']
+        ]
+      },
+      {
+        nodes: [
+          { id: 'X' }, { id: 'Y' }, { id: 'Z' }, { id: 'W' },
+          { id: 'V' }, { id: 'U' }, { id: 'T' }, { id: 'S' }
+        ],
+        edges: [
+          ['X', 'Y'], ['Y', 'Z'], ['Z', 'W'], ['W', 'V'],
+          ['V', 'U'], ['U', 'T'], ['T', 'S'], ['S', 'X']
+        ]
+      },
+      {
+        nodes: [
+          { id: 'M' }, { id: 'N' }, { id: 'O' }, { id: 'P' },
+          { id: 'Q' }, { id: 'R' }, { id: 'S' }, { id: 'T' }
+        ],
+        edges: [
+          ['M', 'N'], ['N', 'O'], ['O', 'P'], ['P', 'Q'],
+          ['Q', 'R'], ['R', 'S'], ['S', 'T'], ['T', 'M']
+        ]
+      },
+      {
+        nodes: [
+          { id: 'I' }, { id: 'J' }, { id: 'K' }, { id: 'L' },
+          { id: 'M' }, { id: 'N' }, { id: 'O' }, { id: 'P' }
+        ],
+        edges: [
+          ['I', 'J'], ['J', 'K'], ['K', 'L'], ['L', 'M'],
+          ['M', 'N'], ['N', 'O'], ['O', 'P'], ['P', 'I']
+        ]
+      }
     ]
+
   }),
  
   mounted() {
-    //let iteration = 0
-    //this.treeNode = this.options[iteration]
-
+    //let iteration = Math.floor(Math.random() * this.options.length)
+    this.treeNode = this.options[5]
     /*setInterval(()=>{
-      iteration += 1
-      //this.treeNode = this.options[iteration%3]
+      iteration = Math.floor(Math.random() * this.options.length)
+      this.treeNode = this.options[iteration]
     }, 1000)*/
   }
 };
--- diagram/src/components/Diagram.vue
+++ diagram/src/components/Diagram.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="contents">
-    <!--<div id="zoom">
-      <span style="margin: 10px">Zoom: {{ zoom }} %</span>
-    </div>
-    <span id="offset">x {{ offset.x }}, y {{ offset.y }}</span>-->
     <canvas 
       id="canvas" 
       :height="canvas.height" 
@@ -12,7 +8,6 @@
       @mousedown.right.prevent="startDragging"
       @mousemove.right="dragging"
       @mouseup.right="stopDragging"
-      @mousedown.left="createNode"
       >
 
     </canvas>
@@ -27,8 +22,11 @@ class Node {
     this.y = null
     this.text = ""
     this.id = Math.floor(Math.random() * 1000000)
+    this.adjacents = []
+    this.adjacentCount = 0
   }
 }
+
 export default {
   name: 'DiagramView',
   props: {
@@ -97,7 +95,7 @@ export default {
     },
     generateTile(x, y, tileSize) {
       this.canvas.ctx.fillStyle = "transparent";
-      this.canvas.ctx.strokeStyle = "black";
+      this.canvas.ctx.strokeStyle = "#222";
       this.canvas.ctx.lineWidth = 0.1;
 
       const halfSize = tileSize / 2;
@@ -126,22 +124,36 @@ export default {
     },
     generateNodes() {
       for (let i=0; i<this.nodes.length; i++) {
-        this.canvas.ctx.fillStyle = "red";
-        this.canvas.ctx.strokeStyle = "red";
-        this.canvas.ctx.lineWidth = 2;
+        this.canvas.ctx.fillStyle = "#555";
+        this.canvas.ctx.strokeStyle = "#555";
+        this.canvas.ctx.lineWidth = 1;
+
+        let width = (this.nodes[i].text.length * Math.floor(6*(this.zoom/100))) + 10
+        let tiles = Math.ceil(width/(this.baseTileSize*this.zoom/100))
 
         this.canvas.ctx.strokeRect(
-          (this.nodes[i].x - .5) * this.baseTileSize * this.zoom/100 + this.canvas.width/2 + this.offset.x + 1,
+          (this.nodes[i].x - tiles*.5) * this.baseTileSize * this.zoom/100 + this.canvas.width/2 + this.offset.x + 1,
           -(this.nodes[i].y+.5) * this.baseTileSize * this.zoom/100 + this.canvas.height/2 + this.offset.y + 1,
-          this.baseTileSize * this.zoom/100 - 1,
+          (this.baseTileSize * this.zoom/100 - 1)*tiles,
           this.baseTileSize * this.zoom/100 - 1
+        );
+
+        this.canvas.ctx.fillStyle = "white";
+        this.canvas.ctx.textAlign = "center";
+        this.canvas.ctx.textBaseline = "middle";
+        this.canvas.ctx.font = Math.floor(12*(this.zoom/100))+"px Arial";
+
+        this.canvas.ctx.fillText(
+            this.nodes[i].text,
+            this.nodes[i].x * this.baseTileSize * this.zoom / 100 + this.canvas.width / 2 + this.offset.x,
+            -this.nodes[i].y * this.baseTileSize * this.zoom / 100 + this.canvas.height / 2 + this.offset.y
         );
       }
     },
     generateEdges() {
-      console.log('Draw Edges!')
-      for (let i=0; i<this.edges.length; i++) {
+      this.edges.map(edge => edge.sort((a, b) => a.x - b.x).sort((a,b) => b.y - a.y))
 
+      for (let i=0; i<this.edges.length; i++) {
         let transformationOrigin = {
           x: 0,
           y: 0, 
@@ -154,28 +166,28 @@ export default {
 
         if (this.edges[i][0].x < this.edges[i][1].x) {
           if (this.edges[i][0].y == this.edges[i][1].y) {
-            transformationOrigin.x = .5
-            transformationDestin.x = -.5
+            transformationOrigin.x = .5*this.edges[i][0].width
+            transformationDestin.x = -.5*this.edges[i][1].width
           }
           else {
-            transformationOrigin.x = 0
-            transformationDestin.x = -.5 
+            transformationOrigin.x = 0*this.edges[i][0].width
+            transformationDestin.x = -.5 *this.edges[i][1].width
           }
         }
 
         else if (this.edges[i][0].x > this.edges[i][1].x) {
           if (this.edges[i][0].y == this.edges[i][1].y) {
-            transformationOrigin.x = -.5
+            transformationOrigin.x = -.5*this.edges[i][0].width
             transformationDestin.x = .5
           }
           else {
-            transformationOrigin.x = 0 
-            transformationDestin.x = .5
+            transformationOrigin.x = 0 *this.edges[i][0].width
+            transformationDestin.x = .5*this.edges[i][1].width
           }
         }
 
         else {
-          transformationDestin.y = 0
+          transformationDestin.y = 0*this.edges[i][1].width
         }
 
         if (this.edges[i][0].y > this.edges[i][1].y) {
@@ -214,7 +226,7 @@ export default {
             this.edges[i][1].y - 1 + transformationDestin.y,
           )
         )
-        this.canvas.ctx.strokeStyle = 'blue';
+        this.canvas.ctx.strokeStyle = '#333';
         this.canvas.ctx.lineWidth = 2;
         this.canvas.ctx.stroke();
 
@@ -231,11 +243,9 @@ export default {
             this.edges[i][1].y - 1 + transformationDestin.y,
           )
         )
-        this.canvas.ctx.strokeStyle = 'blue';
+        this.canvas.ctx.strokeStyle = '#333';
         this.canvas.ctx.lineWidth = 2;
         this.canvas.ctx.stroke();
-
-
       }
     },
     generateGrid() {
@@ -289,7 +299,7 @@ export default {
       this.canvas.ctx = canvas.getContext("2d")
       this.canvas.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-      this.generateGrid()
+      //this.generateGrid()
       //this.generateAxis()
       this.generateEdges()
       this.generateNodes()
@@ -304,8 +314,8 @@ export default {
       node.y = y
       this.nodes.push(node)
 
-      this.canvas.ctx.fillStyle = "red";
-      this.canvas.ctx.strokeStyle = "red";
+      this.canvas.ctx.fillStyle = "#333";
+      this.canvas.ctx.strokeStyle = "#333";
       this.canvas.ctx.lineWidth = 1;
 
       this.canvas.ctx.strokeRect(
@@ -313,8 +323,196 @@ export default {
         this.baseTileSize * this.zoom/100 - 1,
         this.baseTileSize * this.zoom/100 - 1
       );
+    },
+    /*positionNodes() {
+      this.edges = [];
+      this.nodes = [];
 
-      console.log(x,y)
+      const nodes = [...this.treeNode.nodes];
+      const edges = [...this.treeNode.edges];
+
+      // Initialize node positions randomly
+      nodes.forEach(node => {
+        const newNode = new Node();
+          newNode.text = node.id;
+          newNode.x = Math.floor(Math.random() * 18) - 9;
+          newNode.y = Math.floor(Math.random() * 12) - 6;
+          this.nodes.push(newNode);
+        });
+
+      // Fruchterman-Reingold force-directed layout simulation
+      const k = 1.5; // Optimal distance factor
+      const c = 0.2; // Force factor
+      const iterations = 1000;
+
+      for (let iter = 0; iter < iterations; iter++) {
+        // Initialize forces
+        // eslint-disable-next-line no-unused-vars
+        const forces = this.nodes.map(node => ({ x: 0, y: 0 }));
+
+        // Calculate repulsion forces
+        this.nodes.forEach((node1, i) => {
+          this.nodes.forEach((node2, j) => {
+            if (i !== j) {
+              const dx = node2.x - node1.x;
+              const dy = node2.y - node1.y;
+              const distance = Math.max(1, Math.sqrt(dx * dx + dy * dy));
+              const forceX = (dx / distance) * k * k / distance;
+              const forceY = (dy / distance) * k * k / distance;
+
+              forces[i].x -= forceX;
+              forces[i].y -= forceY;
+            }
+          });
+        });
+
+        // Calculate attraction forces
+        edges.forEach(edge => {
+          const node1 = this.nodes.find(node => node.text === edge[0]);
+          const node2 = this.nodes.find(node => node.text === edge[1]);
+
+          const dx = node2.x - node1.x;
+          const dy = node2.y - node1.y;
+          const distance = Math.max(1, Math.sqrt(dx * dx + dy * dy));
+          const forceX = (dx / distance) * distance * c;
+          const forceY = (dy / distance) * distance * c;
+
+          const index1 = this.nodes.findIndex(node => node.text === edge[0]);
+          const index2 = this.nodes.findIndex(node => node.text === edge[1]);
+
+          forces[index1].x += forceX;
+          forces[index1].y += forceY;
+
+          forces[index2].x -= forceX;
+          forces[index2].y -= forceY;
+        });
+
+        // Update node positions
+        this.nodes.forEach((node, i) => {
+          node.x = Math.round(node.x + forces[i].x);
+          node.y = Math.round(node.y + forces[i].y);
+        });
+      }
+
+      const centroid = {
+        x: Math.round(this.nodes.reduce((sum, node) => sum + node.x, 0) / this.nodes.length),
+        y: Math.round(this.nodes.reduce((sum, node) => sum + node.y, 0) / this.nodes.length),
+      };
+
+      // Center the nodes at (0, 0)
+      this.nodes.forEach(node => {
+        node.x -= centroid.x;
+        node.y -= centroid.y;
+      });
+
+      // Create edges using calculated node positions
+      edges.forEach(edge => {
+        const node1 = this.nodes.find(node => node.text === edge[0]);
+        const node2 = this.nodes.find(node => node.text === edge[1]);
+
+        this.edges.push([
+          { x: node1.x, y: node1.y },
+          { x: node2.x, y: node2.y }
+        ]);
+      });
+    }*/
+    incest(node, siblings) {
+      let incestous = []
+      for (let i in siblings) {
+        if (siblings[i] != node) {
+          if (node.adjacents.includes(siblings[i].id)) {
+            incestous.push(siblings[i])
+          }
+        }
+      }
+      return incestous
+    },
+    positionNodes() {
+
+      this.treeNode.nodes.forEach((node)=>{
+        let newNode = new Node()
+        newNode.text = node.id
+        this.nodes.push(newNode)
+      })
+      this.treeNode.edges.forEach((edge)=>{
+        let node1 = this.nodes.filter(node => node.text == edge[0])[0]
+        let node2 = this.nodes.filter(node => node.text == edge[1])[0]
+
+        node1.adjacents.push(node2.id)
+        node2.adjacents.push(node1.id)
+        node2.adjacentCount += 1
+        node1.adjacentCount += 1
+      })
+
+      let traversingNodes = [... this.nodes].sort((node1, node2) => node1.adjacentCount - node2.adjacentCount)
+
+      let row = 0
+      let col = 0
+
+      function drawSubgraph(node, adjacentNodes, nodes, vm) {
+        traversingNodes = traversingNodes.filter(node => !adjacentNodes.includes(node))
+        let carry = 0
+        let placedNodes = []
+
+        for (let i in adjacentNodes) {
+          console.log(adjacentNodes[i].text)
+          console.log(vm.incest(adjacentNodes[i]), adjacentNodes)
+          if (placedNodes.some(node => vm.incest(adjacentNodes[i], adjacentNodes).includes(node))) {
+            console.log(adjacentNodes[i].text)
+            carry += 2
+          }
+          adjacentNodes[i].x = node.x + 2 + Math.floor(node.text.length/5) + carry
+          adjacentNodes[i].y = node.y - i*2
+
+          placedNodes.push(adjacentNodes[i])
+
+          let childAdjacentNodes = nodes.filter(node => adjacentNodes[i].adjacents.includes(node.id))
+          childAdjacentNodes = childAdjacentNodes.filter(node => traversingNodes.includes(node))
+          childAdjacentNodes = childAdjacentNodes.sort((node1, node2) => node1.adjacentCount - node2.adjacentCount)
+          childAdjacentNodes = childAdjacentNodes.sort((node1, node2) => vm.incest(node2, childAdjacentNodes).length - vm.incest(node1, childAdjacentNodes).length)
+
+          if (childAdjacentNodes.length) {
+            drawSubgraph(adjacentNodes[i], childAdjacentNodes, nodes, vm)
+          }
+        }
+      }
+
+      while (traversingNodes.length) {
+        let currentNode = traversingNodes.pop()
+        currentNode.x = col
+        currentNode.y = row
+
+        let adjacentNodes = this.nodes.filter(node => currentNode.adjacents.includes(node.id))
+        adjacentNodes = adjacentNodes.sort((node1, node2) => node1.adjacentCount - node2.adjacentCount)
+        adjacentNodes = adjacentNodes.sort((node1, node2) => this.incest(node2, adjacentNodes).length - this.incest(node1, adjacentNodes).length)
+
+        drawSubgraph(currentNode, adjacentNodes, this.nodes, this)
+      }
+
+      const centroid = {
+          x: Math.round(this.nodes.reduce((sum, node) => sum + node.x, 0) / this.nodes.length),
+          y: Math.round(this.nodes.reduce((sum, node) => sum + node.y, 0) / this.nodes.length),
+      };
+      this.nodes.forEach(node => {
+        node.x -= centroid.x;
+        node.y -= centroid.y;
+      });
+
+      this.treeNode.edges.forEach(edge => {
+        const node1 = this.nodes.find(node => node.text === edge[0]);
+        const node2 = this.nodes.find(node => node.text === edge[1]);
+
+        let width1 = (node1.text.length * Math.floor(6*(this.zoom/100))) + 10
+        let tiles1 = Math.ceil(width1/(this.baseTileSize*this.zoom/100))
+
+        let width2 = (node2.text.length * Math.floor(6*(this.zoom/100))) + 10
+        let tiles2 = Math.ceil(width2/(this.baseTileSize*this.zoom/100))
+
+        this.edges.push([
+          { x: node1.x, y: node1.y, width: tiles1 },
+          { x: node2.x, y: node2.y, width: tiles2 }
+        ]);
+      });
     }
   },
   mounted() {
@@ -331,8 +529,9 @@ export default {
       },
       deep: true,
     },
-    nodes: {
+    treeNode: {
       handler() {
+        this.positionNodes()
         this.generateBoard()
       },
       deep: true,
@@ -353,6 +552,7 @@ export default {
   border: 1px solid rgb(171, 171, 171);
   overflow: visible;
   display: block;
+  background: #121212;
 }
 #zoom {
   position: absolute;
